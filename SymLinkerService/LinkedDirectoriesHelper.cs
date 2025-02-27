826856ea4bca10c5c15260aac90279da8f809d56
Filip <archaikek>
2025-02-27 17:26:50 +0100

    Added ToString method to LinkedDirectoriesHelper.LinkedDirectoryInfo, added time interval key to app.config

---
+++ SymLinkerService/LinkedDirectoriesHelper.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SymLinkerService
+{
+	internal static class LinkedDirectoriesHelper
+	{
+		public static readonly string configPath = Directory.GetCurrentDirectory() + "../../../LinkedDirectories.xml";
+		public static List<LinkedDirectoryInfo> GetLinkedDirectories()
+		{
+			List<LinkedDirectoryInfo> results = new List<LinkedDirectoryInfo>();
+
+			XmlDocument document = new XmlDocument();
+			document.TryLoad(configPath, 100, 20);
+			XmlNode root = document.DocumentElement.SelectSingleNode("/Directories");
+
+			foreach (XmlNode directory in root.ChildNodes) // /Directories/Directory
+			{
+				var newdir = new LinkedDirectoryInfo();
+				var srcAttr = directory.Attributes["source"];
+				var trgAttr = directory.Attributes["target"];
+
+				if (srcAttr is null || trgAttr is null)
+				{
+					continue;
+				}
+				else
+				{
+					newdir.source = srcAttr.Value;
+					newdir.target = trgAttr.Value;
+					foreach (XmlNode ignore in directory.ChildNodes) // /Directories/Directory/Ignore
+					{
+						var nameAttr = ignore.Attributes["name"];
+						if (nameAttr is null)
+						{
+							continue;
+						}
+						else
+						{
+							newdir.ignores.Add(nameAttr.Value);
+						}
+					}
+
+				}
+
+				results.Add(newdir);
+			}
+
+			return results;
+		}
+
+		private static void TryLoad(this XmlDocument document, string path, int maxAttempts, int timeoutBetweenAttempts)
+		{
+			int attempt = 0;
+			while (true)
+			{
+				Console.WriteLine($"Attempt {attempt}");
+				try
+				{
+					document.Load(path);
+				}
+				catch (Exception ex)
+				{
+					if (attempt < maxAttempts)
+					{
+						++attempt;
+						Thread.Sleep(timeoutBetweenAttempts);
+						continue;
+					}
+					else
+					{
+						Console.WriteLine("Failed to access config file.");
+						throw ex;
+					}
+				}
+				break;
+			}
+		}
+		public class LinkedDirectoryInfo
+		{
+			public string source, target;
+			public List<string> ignores = new List<string>();
+
+			public LinkedDirectoryInfo() { }
+			public LinkedDirectoryInfo(string source, string target)
+			{
+				this.source = source;
+				this.target = target;
+			}
+			public override string ToString()
+			{
+				return $"{source}/* -> {target}/*\n" +
+					$"\t excluding:\n{GetIgnoreList()}";
+			}
+			private string GetIgnoreList()
+			{
+				string result = "";
+				foreach (string ignore in ignores)
+				{
+					result += $"\t- {ignore}\n";
+				}
+
+				return result;	
+			}
+		}
+	}
+}
--- SymLinkerService/Service1.cs
+++ SymLinkerService/Service1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +22,18 @@ namespace SymLinkerService
 
 		protected override void OnStart(string[] args)
 		{
+			Console.WriteLine("Starting...");
+
+			int interval = Int32.Parse(ConfigurationManager.AppSettings["Interval"]);
+
+
+			Console.WriteLine("Started!");
 		}
 
 		protected override void OnStop()
 		{
+			Console.WriteLine("Stopping...");
+			Console.WriteLine("Stopped!");
 		}
 
 		public void RunAsConsole(string[] args)
@@ -31,6 +42,10 @@ namespace SymLinkerService
 			Console.WriteLine("Press Enter to exit...");
 			Console.ReadLine();
 			OnStop();
+		}
+		private void UpdateJunctions()
+		{
+
 		}
 	}
 }
